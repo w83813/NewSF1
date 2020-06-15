dd188e0b34c7fcee7e4d9d74ff6df2d80e132d19
Allen.tsai <w83813@gmail.com>
2020-06-15 09:05:32 +0800

    ViewimageActivity memo save

--- app/src/main/java/com/example/miis200/ViewimageActivity.java
+++ app/src/main/java/com/example/miis200/ViewimageActivity.java
@@ -2,6 +2,7 @@ package com.example.miis200;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -14,6 +15,7 @@ import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListPopupWindow;
 import android.widget.Toast;
 
@@ -32,14 +34,15 @@ public class ViewimageActivity extends AppCompatActivity implements NotesRecycle
 
     private Button printer;
     private PhotoView viewimg;
+    private EditText memo;
 
     private RecyclerView mRecyclerView;
     private NotesRecyclerAdapter mNoteRecyclerAdapter;
     private ArrayList<Note> mNotes = new ArrayList<>();
     private Note note;
     private BitmapFactory.Options options;
-    int lastposition=0;
     List<String> list = new ArrayList<String>();
+    private String last_imagepath,now_imagepath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +50,7 @@ public class ViewimageActivity extends AppCompatActivity implements NotesRecycle
         setContentView(R.layout.activity_viewimage);
         mRecyclerView = findViewById(R.id.recycleView);
         viewimg = findViewById(R.id.viewimg);
+        memo = findViewById(R.id.memo);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
@@ -80,7 +84,8 @@ public class ViewimageActivity extends AppCompatActivity implements NotesRecycle
             mNoteRecyclerAdapter.addData(mNoteRecyclerAdapter.getItemCount(),note);
         }
         mRecyclerView.setAdapter(mNoteRecyclerAdapter);
-        autoclick();
+        //autoclick();
+        onNoteClick(0);
 
     }
 
@@ -91,19 +96,28 @@ public class ViewimageActivity extends AppCompatActivity implements NotesRecycle
 
     @Override
     public void onNoteClick(int position) {
-        Log.v(TAG,String.valueOf(mNotes.get(position)));
-        list.add(mNotes.get(position).toString());
-        for (int i=0;i<list.size();i++){
-            Log.v(TAG,String.valueOf(list.get(i)));
-        }
-        String imagepath = mNotes.get(position).toString();
-        Bitmap bitmap = BitmapFactory.decodeFile(imagepath,options);
-        viewimg.setImageBitmap(bitmap);
+
 
         SQLiteDatabase idDB = databaseHelper.getReadableDatabase();
         ContentValues values = new ContentValues();
-        values.put("MEMO",list.get(list.size()-1));
-        idDB.update("TABLE2",values, "PATIENTID=? and IMAGEPATH=?", new String[]{patientid,list.get(list.size()-1)});
+
+        if(list.size() != 0) {
+
+
+            values.put("MEMO", memo.getText().toString());
+            idDB.update("TABLE2", values, "PATIENTID=? and IMAGEPATH=?", new String[]{patientid, list.get(list.size()-1)});
+            Cursor cursor = idDB.query("TABLE2", null, "PATIENTID=? and IMAGEPATH=?", new String[]{patientid, mNotes.get(position).toString()}, null, null, null);
+            cursor.moveToNext();
+            String Memo = cursor.getString(cursor.getColumnIndex("MEMO"));
+            memo.setText(Memo);
+
+        }
+
+        list.add(mNotes.get(position).toString());
+        now_imagepath = mNotes.get(position).toString();
+        Bitmap bitmap = BitmapFactory.decodeFile(now_imagepath, options);
+        viewimg.setImageBitmap(bitmap);
+
     }
 
     ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
@@ -119,12 +133,6 @@ public class ViewimageActivity extends AppCompatActivity implements NotesRecycle
     };
 
 
-    public void autoclick() {
-        String imagepath = mNotes.get(0).toString();
-        Bitmap bitmap = BitmapFactory.decodeFile(imagepath,options);
-        viewimg.setImageBitmap(bitmap);
-        list.add(mNotes.get(0).toString());
-    }
 
 
 }
